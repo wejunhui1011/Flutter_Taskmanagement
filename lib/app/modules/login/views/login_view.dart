22dd247bcaf24d180ed644efaa49bad04988d684
Your Name <you@example.com>
2022-11-30 23:13:36 +0700

    Commit V2

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -11,11 +11,62 @@ class LoginView extends GetView<LoginController> {
     return Scaffold(
       backgroundColor: Colors.blue[100],
       body: Container(
-        margin: EdgeInsets.all(Get.height * 0.1)
+        margin: const EdgeInsets.all(100),
         decoration: BoxDecoration( 
           borderRadius: BorderRadius.circular(50),
-          
-        )
+          color: Colors.white,
         ),
+        child: Row(children: [
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  bottomLeft: Radius.circular(50),
+                ),
+                color: Colors.blue,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+       
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Text('Welcome', 
+          style: TextStyle(
+            color: Colors.white, 
+            fontSize: 65,
+            ),
+          ),
+          Text('Please Sign in',
+          style: TextStyle(
+            color: Colors.white, 
+            fontSize: 30,
+            ),
+          ),
+
+          Text('Start Journey us',
+          style: TextStyle(
+            color: Colors.white, 
+            fontSize: 20,
+            ),
+          ),
+          ]),
+      ),
+    ),
+    Expanded(
+        child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+        topRight: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+        color: Colors.white,
+        ),
+        )),
+        ]),
+    ),
+    );
   }
 }
